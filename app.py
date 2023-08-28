8206545a108f2d1dc647a80ddb33367bb043d11a
joryaya99 <110695145+joryaya99@users.noreply.github.com>
2023-08-28 16:52:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
-
+from datetime import datetime
+from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
