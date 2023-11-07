e852204a3b884196e61dbbda5a25a86427ceba41
joryaya99 <110695145+joryaya99@users.noreply.github.com>
2023-11-07 12:27:51 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,23 +8,26 @@ from models import Base, session, Products, engine
 Base.metadata.create_all(engine)
 
 
-def clean_date(date_str):
-    try:
-        date = datetime.datetime.strptime(date_str, '%B %d, %Y').date()
-        return date
-    except ValueError:
-        print("\n****** DATE ERROR ******")
-        print("The date format should be: Month Day, Year (Ex: January 13, 2003)")
-        return None
-    
 def clean_price(price_str):
     try:
-        price_str = price_str.replace(',', '')
+        # Remove commas and spaces, and then convert to a float
+        price_str = price_str.replace(',', '').strip()
         price = int(float(price_str) * 100)
         return price
     except ValueError:
         print("\n****** PRICE ERROR ******")
         print("The price should be a valid number without a currency symbol (Ex: 10.99)")
+        return None
+
+def clean_date(date_str):
+    try:
+        # Remove leading spaces and convert to the correct date format
+        date_str = date_str.strip()
+        date = datetime.datetime.strptime(date_str, '%m/%d/%Y').date()
+        return date
+    except ValueError:
+        print("\n****** DATE ERROR ******")
+        print("The date format should be: Month Day, Year (Ex: 9/14/2022)")
         return None
 
 def read_csv(filename='inventory.csv'):
@@ -33,17 +36,34 @@ def read_csv(filename='inventory.csv'):
         reader = csv.DictReader(csvfile)
         for row in reader:
             try:
+                price = None
+                for price_key in ['product_price', ' product_price']:
+                    price = row.get(price_key.strip(), None)
+                    if price is not None:
+                        price = clean_price(price.strip())
+                        break
+
+                date = None
+                for date_key in ['date_updated', ' date_updated']:
+                    date = row.get(date_key.strip(), None)
+                    if date is not None:
+                        date = clean_date(date.strip())
+                        break
+
+                if price is None or date is None:
+                    raise ValueError("Product price or date not found")
+
                 product = {
                     'product_name': row['product_name'],
                     'product_quantity': int(row.get('product_quantity', 0)),
-                    'product_price': clean_price(row['product_price'].strip()),  # Remove leading and trailing spaces
-                    'date_updated': clean_date(row['date_updated'])
+                    'product_price': price,
+                    'date_updated': date
                 }
                 products_list.append(product)
-            except KeyError as e:
-                print(f"KeyError: {e} in CSV row, skipping the row.")
+            except (KeyError, ValueError) as e:
+                print(f"Error in CSV row, skipping the row: {e}")
                 print(f"Problematic row: {row}")
-                continue  # Skip the row with missing 'product_price'
+
     return products_list
 
 
